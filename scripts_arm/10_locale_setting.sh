a2ad747582b26e3abfec6eededc8f01e8c8a19d0
Mariusz Woloszyn <emsi@users.noreply.github.com>
2021-02-06 21:07:43 +0000

    script setting locale

---
+++ scripts_arm/10_locale_setting.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -Eeuo pipefail
+
+echo "Setting keyboard to '${PI_KEYMAP}'"
+sed -i /etc/default/keyboard -e "s/^XKBLAYOUT.*/XKBLAYOUT=\"$PI_KEYMAP\"/"
+
+echo "Setting locale to '${PI_LOCALE}'"
+echo "$PI_LOCALE" > /etc/locale.gen
+sed -i "s/^\s*LANG=\S*/LANG=$PI_LOCALE/" /etc/default/locale
+
+echo "Setting timezone to '${PI_TIMEZONE}'"
+echo "$PI_TIMEZONE" > /etc/timezone
+dpkg-reconfigure -f noninteractive tzdata
+
