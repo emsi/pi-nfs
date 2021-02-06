f12c184ac5df029a7633597537a5a53041e2abce
Mariusz Woloszyn <emsi@users.noreply.github.com>
2021-02-06 19:21:10 +0000

    Don't install if already present

--- scripts/00_ubuntu_server_configure.sh
+++ scripts/00_ubuntu_server_configure.sh
@@ -1,19 +1,28 @@
-#!/bin/sh
+#!/bin/bash
+set -Eeuo pipefail
 
-# Install atftp
-apt install -y atftpd
-sed -I -e 's/^USE_INETD=true/USE_INETD=false/g' /etc/default/atftpd
-sudo systemctl enable atftpd
-sudo systemctl restart atftpd
+if [[ ! -x /usr/sbin/atftpd ]]; then
+	# Install atftp
+	apt install -y atftpd
+fi
 
-# Install nfs server
-apt-get install -y nfs-kernel-server
+if ! grep -q "USE_INETD=false" /etc/default/atftpd; then
+	sed -i -e 's/^USE_INETD=true/USE_INETD=false/g' /etc/default/atftpd
+	sudo systemctl enable atftpd
+	sudo systemctl restart atftpd
+fi
 
-systemctl enable rpcbind
-systemctl restart rpcbind
-systemctl enable nfs-kernel-server
-systemctl restart nfs-kernel-server
-
-
-# Install qemu for running arm binaries
-apt install -y qemu-user-static binfmt-support
+if [[ ! -x /sbin/rpcbind || ! -x /usr/sbin/rpc.mountd ]]; then
+	# Install nfs server
+	apt-get install -y nfs-kernel-server
+	
+	systemctl enable rpcbind
+	systemctl restart rpcbind
+	systemctl enable nfs-kernel-server
+	systemctl restart nfs-kernel-server
+fi
+		
+if [[ ! -x /usr/bin/qemu-arm-static ]]; then
+	# Install qemu for running arm binaries
+	apt install -y qemu-user-static binfmt-support
+fi
