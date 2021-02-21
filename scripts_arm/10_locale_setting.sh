ac9608959515f681dfc8cb1ddf54292b869f9015
Mariusz Woloszyn <emsi@users.noreply.github.com>
2021-02-21 17:03:21 +0000

    fix timezone setting bug

--- scripts_arm/10_locale_setting.sh
+++ scripts_arm/10_locale_setting.sh
@@ -3,13 +3,14 @@
 set -Eeuo pipefail
 
 echo "Setting keyboard to '${PI_KEYMAP}'"
-sed -i /etc/default/keyboard -e "s/^XKBLAYOUT.*/XKBLAYOUT=\"$PI_KEYMAP\"/"
+sed -i /etc/default/keyboard -e "s/^XKBLAYOUT.*/XKBLAYOUT=\"${PI_KEYMAP}\"/"
 
 echo "Setting locale to '${PI_LOCALE}'"
-echo "$PI_LOCALE" > /etc/locale.gen
+echo "${PI_LOCALE}" > /etc/locale.gen
 sed -i "s/^\s*LANG=\S*/LANG=$PI_LOCALE/" /etc/default/locale
 
 echo "Setting timezone to '${PI_TIMEZONE}'"
-echo "$PI_TIMEZONE" > /etc/timezone
-dpkg-reconfigure -f noninteractive tzdata
-
+echo "${PI_TIMEZONE}" > /etc/timezone
+rm /etc/localtime
+ln -sf "/usr/share/zoneinfo/${PI_TIMEZONE}" /etc/localtime
+dpkg-reconfigure --frontend noninteractive tzdata
